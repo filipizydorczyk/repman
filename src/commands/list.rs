6b81c9e54a5bba854c21a10a77cfccd8e4f3a85b
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-07-24 03:30:48 +0200

    Added config to keep storge folder info

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -5,6 +5,9 @@ use std::fs;
 
 pub const COMMAND: &str = "list";
 
+#[cfg(not(debug_assertions))]
+use crate::config::STORAGE_DIR;
+
 pub fn create_command() -> App<'static> {
     let app = App::new(COMMAND).about("Lists repository that was added to track with repman.");
     app
@@ -15,7 +18,7 @@ pub fn command_handler() {
     let paths = fs::read_dir(".").unwrap();
 
     #[cfg(not(debug_assertions))]
-    let paths = fs::read_dir(dirs::home_dir().unwrap()).unwrap();
+    let paths = fs::read_dir(dirs::home_dir().unwrap().join(STORAGE_DIR)).unwrap();
 
     for path in paths {
         println!("{}", path.unwrap().path().display())
--- src/config.rs
+++ src/config.rs
@@ -2,3 +2,6 @@ pub const NAME: &str = "repman";
 pub const AUTHOR: &str = "Filip Izydorczyk <filip.izydorczyk@protonmail.com>";
 pub const VER: &str = "1.0";
 pub const DESCRIPTION: &str = "Software to manage updates of softwares that I build from source.";
+
+#[cfg(not(debug_assertions))]
+pub const STORAGE_DIR: &str = ".repman";
