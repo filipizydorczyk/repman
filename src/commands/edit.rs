654ae00ba33dd584e5235c48758c06f0eace11b8
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-08-01 23:37:53 +0200

    Added edit command

---
+++ src/commands/edit.rs
@@ -0,0 +1,32 @@
+use clap::{App, Arg, ArgMatches};
+
+use crate::directorystorage;
+
+const COMMAND_DESCRIPTION: &str = "Opens config file for given repository in default editor. U need to have variable EDITOR set so that this command worked. U can just edit file in ~/.repman instead.";
+const SUB_COMMAND_DESRIPTION: &str = "Name of repository to open config file.";
+
+pub const COMMAND: &str = "edit";
+pub const SUB_COMMAND_PATH: &str = "name";
+
+pub fn create_command() -> App<'static> {
+    let app = App::new(COMMAND).about(COMMAND_DESCRIPTION).arg(
+        Arg::new(SUB_COMMAND_PATH)
+            .about(SUB_COMMAND_DESRIPTION)
+            .required(true),
+    );
+    app
+}
+
+pub fn command_handler(matches: &ArgMatches) {
+    let process = std::process::Command::new("nano")
+        .arg(format!(
+            "{}/{}.sh",
+            directorystorage::get_storage_path().to_str().unwrap(),
+            matches.value_of(SUB_COMMAND_PATH).unwrap()
+        ))
+        .spawn();
+
+    match process.unwrap().wait_with_output() {
+        _ => (),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,14 @@ fn main() {
         .subcommand(commands::list::create_command())
         .subcommand(commands::add::create_command())
         .subcommand(commands::remove::create_command())
+        .subcommand(commands::edit::create_command())
         .get_matches();
 
     match matches.subcommand() {
         Some((commands::list::COMMAND, _)) => commands::list::command_handler(),
         Some((commands::add::COMMAND, matches)) => commands::add::command_handler(matches),
         Some((commands::remove::COMMAND, matches)) => commands::remove::command_handler(matches),
+        Some((commands::edit::COMMAND, matches)) => commands::edit::command_handler(matches),
         None => println!("No subcommand was used"),
         _ => unreachable!(),
     }
