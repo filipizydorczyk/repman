83eab1777c02462266fa2148784d0033bc86ffca
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-09-15 18:19:11 +0200

    check command accepts now multiple arguments

--- src/commands/check.rs
+++ src/commands/check.rs
@@ -14,26 +14,36 @@ pub fn create_command() -> App<'static> {
     let app = App::new(COMMAND).about(COMMAND_DESCRIPTION).arg(
         Arg::new(SUB_COMMAND_PATH)
             .about(SUB_COMMAND_DESRIPTION)
-            .required(true),
+            .required(false)
+            .min_values(0),
     );
     app
 }
 
 pub fn command_handler(matches: &ArgMatches) {
-    let repo_name = matches.value_of(SUB_COMMAND_PATH).unwrap();
-    let header = format!("{} {}", "Changes for".bold(), repo_name.yellow());
+    let args = matches.values_of(SUB_COMMAND_PATH);
 
-    println!("\n{}", header);
-    println!("{}", "=".repeat(header.chars().count() - 17));
+    let repos: Vec<_> = match args {
+        None => Vec::new(),
+        _ => args.unwrap().collect(),
+    };
 
-    std::process::Command::new("bash")
-        .arg("-c")
-        .arg(format!(
-            "source {}/{}.sh && updateCheck",
-            directorystorage::get_storage_path().to_str().unwrap(),
-            repo_name
-        ))
-        .stdout(Stdio::inherit())
-        .output()
-        .expect("Failed to execute updateCheck function");
+    repos.iter().for_each(|repo| {
+        let repo_name = repo;
+        let header = format!("{} {}", "Changes for".bold(), repo_name.yellow());
+
+        println!("\n{}", header);
+        println!("{}", "=".repeat(header.chars().count() - 17));
+
+        std::process::Command::new("bash")
+            .arg("-c")
+            .arg(format!(
+                "source {}/{}.sh && updateCheck",
+                directorystorage::get_storage_path().to_str().unwrap(),
+                repo_name
+            ))
+            .stdout(Stdio::inherit())
+            .output()
+            .expect("Failed to execute updateCheck function");
+    });
 }
