db21f644201dd7f0fc6d77a5708c5324aaeae1f1
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-12-03 21:23:40 +0100

    feat: added multiple arguments to install command

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -15,26 +15,37 @@ pub fn create_command() -> App<'static> {
     let app = App::new(COMMAND).about(COMMAND_DESCRIPTION).arg(
         Arg::new(SUB_COMMAND_PATH)
             .about(SUB_COMMAND_DESRIPTION)
-            .required(true),
+            .required(false)
+            .min_values(0),
     );
     app
 }
 
 pub fn command_handler(matches: &ArgMatches) {
-    let repo_name = matches.value_of(SUB_COMMAND_PATH).unwrap();
-    let header = format!("{} {}", "Installing".bold(), repo_name.yellow());
+    let repo_names = matches.values_of(SUB_COMMAND_PATH);
+    let repos: Vec<String> = match repo_names {
+        None => directorystorage::get_stored_repositories_names(),
+        _ => repo_names
+            .unwrap()
+            .map(|element| -> String { String::from(element) })
+            .collect(),
+    };
 
-    println!("\n{}", header);
-    println!("{}", "=".repeat(header.chars().count() - 17));
-    std::process::Command::new("bash")
-        .arg("-c")
-        .arg(format!(
-            "source {}/{}.sh && {}",
-            directorystorage::get_storage_path().to_str().unwrap(),
-            repo_name,
-            INSTALL_COMMAND_NAME
-        ))
-        .stdout(Stdio::inherit())
-        .output()
-        .expect("Intallation failed");
+    repos.iter().for_each(|repo_name| {
+        let header = format!("{} {}", "Installing".bold(), repo_name.yellow());
+
+        println!("\n{}", header);
+        println!("{}", "=".repeat(header.chars().count() - 17));
+        std::process::Command::new("bash")
+            .arg("-c")
+            .arg(format!(
+                "source {}/{}.sh && {}",
+                directorystorage::get_storage_path().to_str().unwrap(),
+                repo_name,
+                INSTALL_COMMAND_NAME
+            ))
+            .stdout(Stdio::inherit())
+            .output()
+            .expect("Intallation failed");
+    });
 }
